c583da230a6759a5a94a9e36655951a4d725bd09
Rodrigo Silva <rodrigo.7762018@gmail.com>
2025-08-29 15:01:45 -0300

    my first commit

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-        // 2 barras geram comentarios em JS
-        // comentarios com mais de 1 linha se usa /* */
+    // 2 barras geram comentarios em JS
+       // comentarios com mais de 1 linha se usa /* */
 
-        var variavel;
-        variavel = 3 + 3;
+    var variavel;
+    variavel = 3 + 3;
 
-        alert(variavel);
+    alert(variavel);
 
-        var resultadoMultiplicacao = multiplique(10, 50);
+    var resultadoMultiplicacao = multiplique(10, 50);
 
-        var divLocal = document.getElementById('exibe_resultado');
-        divLocal.innerHTML += resultadoMultiplicacao;
+    var divLocal = document.getElementById('exibe_resultado');
+    divLocal.innerHTML += resultadoMultiplicacao;
 
-        function multiplique(numero1, numero2){
-            var resultado = 0;
+    function multiplique(numero1, numero2){
+        var resultado = 0;
 
-            resultado = numero1 * numero2;
+        resultado = numero1 * numero2;
 
-            return resultado;
-        }
+        return resultado;
+    }
